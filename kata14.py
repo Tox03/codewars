39a0621f308a9ac08fcf11950b24347bc10648d9
Tox03 <89782484+Tox03@users.noreply.github.com>
2023-07-24 23:47:52 +0300

    Delete kata14.py

--- kata14.py
+++
@@ -1,9 +0,0 @@
-def perimeter(n):  # сумма чисел фибоначи до n+1 элемента умноженная на 4
-    temporal = [1, 1]
-    n -= 1
-    for i in range(n):
-        temporal.append(temporal[i + 1] + temporal[i])
-    return sum(temporal)*4
-
-
-print(perimeter(500))
